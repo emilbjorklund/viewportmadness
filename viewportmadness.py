d808413fd180b8a69c2cb102b03d5036a7bab732
Emil Björklund <emil.bjorklund@inuse.se>
2012-09-14 13:52:57 +0200

    Got it working with Landscape as well.

--- viewportmadness.py
+++ viewportmadness.py
@@ -1,10 +1,10 @@
 import csv
 
-deviceData = csv.reader(open('device-data.csv'))
+devicefile = 'device-data.csv'
+cssfile = 'viewportmadness.css'
 
 widthDict = {}
 
-print "Hello!"
 """
 for row in deviceData:
     print row
@@ -12,35 +12,44 @@ for row in deviceData:
 
 
 def descr_joiner(row, format):
-    return '; '.join((row[0], row[1], row[2], format))
+    return ', '.join((row[0], row[1], row[2], format))
 
 
 def loop_and_pick(idx, format, target):
+    deviceData = csv.reader(open(devicefile))
     for row in deviceData:
+        # get a normalized ref to the width number
+        width = str(row[idx]).strip().split(' ')[0]
+        print width
 
-        if (row[idx] != format.strip()) or (row[idx].strip() != 'NA'):  # skip the header line and NA-lines
-            if row[idx] not in target:  # New entry, create object in deviceData:
-                # print "New width: %s" % row[idx]
-                target[row[idx]] = {
-                    'devices': [descr_joiner(row, format)]  # add the device specific names as a string
-                }
-            else:  # Already has that measurement, just append to list of devices.
-                # print "Already have that one!"
-                target[row[idx]]['devices'].append(descr_joiner(row, format))
+        if (width != format) or (width != 'NA'):  # skip the header line and NA-lines
+            try:
+                width = int(width)
+                if width not in target:  # New entry, create object in deviceData:
+                    # print "New width: %s" % row[idx]
+                    target[width] = {
+                        'devices': [descr_joiner(row, format)]  # add the device specific names as a string
+                    }
+                else:  # Already has that measurement, just append to list of devices.
+                    # print "Already have that one!"
+                    target[width]['devices'].append(descr_joiner(row, format))
+            except ValueError:
+                pass
         else:
             pass
             # print "This line is a goner."
-
+    return target
 
 loop_and_pick(3, 'Portrait', widthDict)
+print 'Loop again, adding Landscape'
 loop_and_pick(4, 'Landscape', widthDict)
 
 
-with open('viewportmadness.css', 'w') as f:
+with open(cssfile, 'w') as f:
 
     for item in sorted(widthDict.iterkeys()):
         lines = [
-            '/* %s */\n' % (", ".join(widthDict[item]['devices'])),
+            '/* %s */\n' % ("; ".join(widthDict[item]['devices'])),
             '@media screen and (min-width: %spx) {\n' % item,
             '    /* stuff for %s and up */\n' % item,
             '}\n\n',
